d3cb76b498f6a2957e66d194f31893acb6928bb8
Yuri Stepin <enchantinggg4@gmail.com>
2025-02-15 12:49:22 +0300

    Fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,6 +33,7 @@ import { getTypeormConfig } from './config/typeorm.config';
           database: 'postgres',
           migrations: ['dist/database/migrations/*.*'],
           migrationsRun: true,
+          logging: undefined,
         } satisfies TypeOrmModuleOptions;
       },
       imports: [],
