e776ad3c9f73a0ce9bc054d8c9b9ab8096f49020
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-22 12:42:28 +0300

    Redlock

--- src/forum/thread-stats.service.ts
+++ src/forum/thread-stats.service.ts
@@ -3,6 +3,7 @@ import { ThreadEntity } from './model/thread.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import Redlock from 'redlock';
 
 @Injectable()
 export class ThreadStatsService implements OnApplicationBootstrap {
@@ -11,20 +12,27 @@ export class ThreadStatsService implements OnApplicationBootstrap {
   constructor(
     @InjectRepository(ThreadEntity)
     private readonly threadEntityRepository: Repository<ThreadEntity>,
+    private readonly redlock: Redlock,
   ) {}
 
   @Cron(CronExpression.EVERY_MINUTE)
   public async refreshView() {
-    try {
-      await this.threadEntityRepository.query(
-        `REFRESH MATERIALIZED VIEW thread_stats_view;`,
-      );
-      await this.threadEntityRepository.query(
-        `REFRESH MATERIALIZED VIEW last_message_view_materialized;`,
-      );
-    } catch (e) {
-      this.logger.warn('Error updating views');
-    }
+    await this.redlock.using(
+      [`${ThreadStatsService.name}_materialized_views`],
+      10000,
+      async (signal) => {
+        await this.threadEntityRepository.query(
+          `REFRESH MATERIALIZED VIEW thread_stats_view;`,
+        );
+        await this.threadEntityRepository.query(
+          `REFRESH MATERIALIZED VIEW last_message_view_materialized;`,
+        );
+        if (signal.aborted) {
+          throw signal.error;
+        }
+        this.logger.log('Updated views');
+      },
+    );
   }
 
   async onApplicationBootstrap() {
